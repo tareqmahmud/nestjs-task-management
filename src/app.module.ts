49945b909a87254b16f72a657c2d7ab74df986a1
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-01-01 15:50:20 +0600

    Generate the model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/config.typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, UsersModule],
 })
 export class AppModule {}
