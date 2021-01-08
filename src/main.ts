1521f690672abf7c6a495078b82d7816349ff839
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-01-08 16:13:59 +0600

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,16 @@ import * as config from 'config';
 async function bootstrap() {
   const appLogger = new Logger('app');
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: process.env.CORS_ORIGIN,
+    });
+  }
+
+  // Cors
   appLogger.log('App is on fire');
 
   const appPort = process.env.APPLICATION_PORT || config.get('server.port');
